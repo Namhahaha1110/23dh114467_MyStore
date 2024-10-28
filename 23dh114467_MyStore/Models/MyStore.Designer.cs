ff9b5e55c0e1f8a6d0cf40d4b8a8abdca12816bf
Namhahaha1110 <23dh114467@st.huflit.edu.vn>
2024-10-28 10:51:36 +0700

    sql update

--- 23dh114467_MyStore/Models/MyStore.Designer.cs
+++ 23dh114467_MyStore/Models/MyStore.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'D:\LeHoangNam_23dh114467\23dh114467_MyStore\23dh114467_MyStore\Models\MyStore.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\SV\23dh114467_MyStore\23dh114467_MyStore\Models\MyStore.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
